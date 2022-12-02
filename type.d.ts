37d15f81fb7fe4fc2432a16aaa73720968fe9b3b
Alinka2906 <418420a@gmail.com>
2022-12-02 20:32:03 +0600

    Сщзданы и сверстаны компоненты: NavBar, Post,type.d.ts, Home, Contacts, axiosApi, AddForm, About. Написыны стили

---
+++ type.d.ts
@@ -0,0 +1,14 @@
+export interface EnterPosts {
+  id?: string;
+  date: string;
+  name: string;
+  title: string;
+  descriptions:string;
+}
+
+export interface PostsMutation {
+  date: number;
+  name: string;
+  title: string;
+  descriptions:string;
+}
